3ed51da4389d46da23965c771abf6ad2b13175f7
Jagerud <jaegerud@gmail.com>
2018-01-16 15:54:29 +0100

    started algoritm

--- src/start/Sorting.java
+++ src/start/Sorting.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
  */
 public class Sorting {
     private InputClass inputClass;
+    private final String removed = "xxxxx";
 
     public Sorting(InputClass inputClass){
         this.inputClass = inputClass;
@@ -16,15 +17,31 @@ public class Sorting {
     public void sort(){
 
         HashMap boxMap = inputClass.getBoxMap();
-        HashMap maps = inputClass.getMaps();
-        ArrayList boxNam = inputClass.getBoxOnTopList();
+        HashMap <String, ArrayList<String>> maps = inputClass.getMaps();
+        ArrayList <String> boxNam = inputClass.getBoxOnTopList();
         System.out.println(inputClass.getNrOfBoxes());
         //move if nothing above
+        //maps.put("l","");
+        maps.get("i").clear();
         for(int i=0;i<inputClass.getNrOfBoxes();i++) {
-            if(!maps.containsKey(boxNam.get(i)) /*|| (maps.get(boxNam.get(i).))*/){ //check if box does not exist in order
-                //move the stuff
-                System.out.println("inside if");
+            if(!maps.containsKey(boxNam.get(i)) || (maps.get(boxNam.get(i)).isEmpty()) || !boxNam.get(i).matches(removed)){ //check if box does not exist in order
+                //Remove boxes, they have nothing on them
+                System.out.println("inside 1 if");
+                boxNam.get(i).replaceAll(boxNam.get(i), "*****");   //Changes removed box´s name to ***** to skip
+                for(int j=0;j<inputClass.getNrOfBoxes();j++) { //TODO fix with box
+                    if(maps.containsValue("")){
+                        //If box to be removed is value, remove value
+                        if(!maps.containsKey(boxNam.get(j)) || (maps.get(boxNam.get(j)).isEmpty())){ //If now empty
+                            //Remove boxes, they have nothing on them
+                            boxNam.get(j).replaceAll(boxNam.get(j), removed);   //Changes removed box´s name to ***** to skip
+                        }
+                    }
+
+
+                }
+
             }else{
+                //Still under a box
                 System.out.println("outside if");
             }
         }
